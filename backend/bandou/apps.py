32ed6e1031393f6cd8dbdcf3ba1cdfb53b1d88df
qinyingdong <qinyingdong@qq.com>
2025-06-26 20:45:47 +0800

    temp drop backend snapshot to graft history

--- backend/bandou/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BandouConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "bandou"
-
-    def ready(self):
-        import bandou.utils.signals  # noqa: F401
--- backend/bandou/utils/get_redis_instance.py
+++
@@ -1,8 +0,0 @@
-import redis
-from django.conf import settings
-
-
-def get_redis_instance():
-    """从redis连接池中获取redis实例"""
-    pool = redis.ConnectionPool(**settings.REDIS_CONFIG)
-    return redis.Redis(connection_pool=pool)
